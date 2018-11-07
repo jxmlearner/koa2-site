fa9a004984d3da29545176f68a144b8942e02d08
jxmlearner <410958040@qq.com>
2018-11-07 11:45:27 +0800

    用户设置昵称进入聊天室

--- src/public/scripts/chat.js
+++ src/public/scripts/chat.js
@@ -1,5 +1,27 @@
 $(function(){
     var socket=io()
+    var onLineUsers=[]
+    var currentUser=null
+
+    socket.on('connect', () => {
+        console.log(socket.id)
+        socket.emit('check if olduser')
+        socket.emit('get online users')   //去获取在线用户列表
+    })
+
+    socket.on('check if olduser',(flag)=>{
+        if(flag){   //说明是已经存在的用户,现在是重新连接上服务器
+            return
+        }
+        //弹出输入用户名的modal
+        $('#myModal').modal({
+            backdrop: 'static',
+            keyboard: false
+        })
+    })
+    socket.on('get online users',users=>{
+        setOnLineUserList(users)
+    })
 
     socket.on('chat message',function(msg){
         console.log(msg);
@@ -7,7 +29,61 @@ $(function(){
     })
 
     socket.on('leave chat',function(leaveUser){
-        toastr.error(leaveUser+'离开了!')
+        if(leaveUser){
+            toastr.error(leaveUser.name+'离开了!')
+        }        
     })
 
+    socket.on('notify user login',loginuser=>{
+        toastr.info(`用户${loginuser.name}上线了`)
+    })
+
+    //设置左侧在线用户列表
+    function setOnLineUserList(users){
+        console.log(users)
+        onLineUsers=users
+        var html=users.reduce((cur,next)=>{
+            var temp=`<li><a href="javascript:void(0);" title="${next.name}"><img src="${next.img}" class="img-thumbnail"><span>${next.name}</span></a></li>`;
+            return cur+=temp;
+        },'')
+        $("#onLineUsers").html(html);
+    }
+
+    //login
+	$('#btn-setName').click(function(){
+		var name = $('#username').val();
+
+        if(!name) {toastr.warning('昵称不能为空！'); return}
+		if(checkUser(name)){
+			$('#username').val('');
+			toastr.warning('昵称已经存在！');
+		}else{
+			var imgList = ["/images/1.jpg","/images/2.jpg","/images/3.jpg","/images/4.jpg","/images/5.jpg"];
+			var randomNum = Math.floor(Math.random()*5);
+			//random user
+			var img = imgList[randomNum];
+			//package user
+			var dataObj = {
+                id:socket.id,
+				name:name,
+				img:img
+			};
+			//send user info to server
+			socket.emit('login',dataObj);
+			//hide login modal
+			$('#myModal').modal('hide');
+			$('#username').val('');
+            $('#msg').focus();
+            setCurrentUser(dataObj)
+		}
+    });
+    //check is the username exist.
+    function checkUser(name){
+        var findUser=onLineUsers.find(u=>u.name==name);
+        return findUser?true:false;
+    }
+    function setCurrentUser(user){
+        currentUser=user;
+        $("#spanuser").html(`欢迎您！ ${user.name}`)
+    }
 })
--- src/socket/chat.js
+++ src/socket/chat.js
@@ -1,5 +1,13 @@
 const socketIO = require('socket.io')
 
+class User{
+    constructor(id,name,img){
+        this.id=id
+        this.name=name              //用户名
+        this.img=img                //用户头像
+    }
+}
+
 
 class chat {
     constructor(http) {
@@ -13,13 +21,31 @@ class chat {
         let that=this;
         io.on('connection', socket => {
             //有一个用户连接过来了
-            let onLineUsersCount=this.onLineUsers.size;
-            if(!this.onLineUsers.has(socket.id)){
-                this.onLineUsers.set(socket.id,'游客'+(onLineUsersCount+1))
-            }
+            
 
-            console.log(this.onLineUsers);
+            //检查是否是已经存在的用户
+            socket.on('check if olduser',function(){
+                var user=that.onLineUsers.get(socket.id)
+                //var users=user?[...that.onLineUsers.values()]:[]
+                socket.emit('check if olduser',user?true:false)
+            })
 
+            //取在线用户列表
+            socket.on('get online users',function(){
+                socket.emit('get online users',[...that.onLineUsers.values()])
+            })
+
+            //用户登录
+            socket.on('login',user=>{
+                //将用户添加至在线用户列表中
+                user.id=socket.id
+                that.onLineUsers.set(socket.id,user)
+                //接着触发客户端去重新获取用户列表  --通知所有客户端
+                io.emit('get online users',[...that.onLineUsers.values()])
+
+                //给其它的客户端发送通知,我上线了
+                socket.broadcast.emit('notify user login',user)
+            })
             
 
             socket.on('disconnect', function () {
